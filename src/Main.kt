860fab9f2d621da8dbfb9cb7a691ac8d9b6c61e9
cathernekaninni <catherinekaninni@gmail.com>
2022-03-04 16:57:28 +0300

    add changes made

--- src/Main.kt
+++ src/Main.kt
@@ -1,30 +1,31 @@
 fun main(){
-    String()
-    nameage()
-    length()
+    var come = String("AkiraChix")
+    println (come)
+    var word = nameage("Catherine Kanini",20)
+    println(word)
+    var greet = length("AkiraChix")
+    println(greet)
     ifelse("Catherine Kilonzi")
 
 }
 /*Given a String "AkiraChix".Write a function that prints out a String composed of the first,third and
 fourth characters of the String*/
-fun String(){
-    var name = "AkiraChix"
-    println(name[1].toString()+name[3]+name[4])
+fun String(name:String):String{
+    var stmt = (name[1].toString()+name[3]+name[4])
+    return stmt
 
 
 }
 /*Write a function that takes in 2 parameter's,name and age and return a String with this structure "Hi,my
 name x and i am y years old"*/
-fun nameage(){
-    var name = "Catherine"
-    var age = 20
-    var statement = "Hi,my name is $name and am $age years old"
-    println(statement)
+fun nameage(name:String,age:Int):String{
+    var statement = "Hi,my name is $name and am ${age.toString()} years old"
+    return statement
 }
 /*Write a function that takes in a String and returns its length*/
-fun length(){
-    var name = "AkiraChix"
-    println(name.length)
+fun length(name:String):Int{
+    var name = name.length
+    return name
 }
 /*Write a function that takes in a name and prints out "That's me!"when your name is passed to it,otherwise
 it prints"I don't know who that is"*/
